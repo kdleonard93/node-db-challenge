be65c94ebbcd638c3e15bfa075916bb8c240b9d5
Kyle Leonard <kdleonard@icloud.com>
2020-03-08 18:47:00 -0500

    Progress with Migrations

--- migrations/20200308180006_initial.js
+++ migrations/20200308180006_initial.js
@@ -1,3 +1,23 @@
-exports.up = async function(knex) {};
+exports.up = async function(knex) {
+  await knex.schema.createTable("projects", table => {
+    table.increment("id").primaryKey();
+    table.text("name").notNull();
+    table.text("description");
+    table.boolean("completed").notNull(false);
+  });
+
+  await knex.schema.createTable("resource", table => {
+    table.increment("id").unique();
+    table.text("name").notNull();
+    table.text("description");
+  });
+
+  await knex.schema.createTable("tasks", table => {
+    table.increment("id").unique();
+    table.text("description").notNull();
+    table.text("notes");
+    table.boolean("completed").notNull(false);
+  });
+};
 
 exports.down = async function(knex) {};
