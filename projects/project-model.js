bcd3521e7338ec0e79465b6864110da0e8292a45
Kyle Leonard <kdleonard@icloud.com>
2020-03-08 20:35:30 -0500

    Worked on project model and router

---
+++ projects/project-model.js
@@ -0,0 +1,28 @@
+const knex = require("knex");
+const db = require("../data/config");
+module.exports = {
+  find,
+  findByID,
+  add,
+  remove
+};
+
+function find() {
+  return db("projects");
+}
+
+function findByID(id) {
+  return db("projects")
+    .where({ id })
+    .first();
+}
+
+function add(project) {
+  return db("projects").insert(project);
+}
+
+function remove() {
+  return db("projects")
+    .where({ id })
+    .del();
+}
