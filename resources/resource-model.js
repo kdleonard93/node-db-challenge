a69117e3c0f373bb63a71d237b10b94fbaaefdfb
Kyle Leonard <kdleonard@icloud.com>
2020-03-09 12:20:05 -0500

    Completed resource model

---
+++ resources/resource-model.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const knex = require("../data/config");
+
+module.exports = {
+  find,
+  findByID,
+  add
+};
+
+function find() {
+  return db("resources");
+}
+
+function findByID(id) {
+  return db("resources")
+    .where({ project_id })
+    .first();
+}
+
+function add(resources) {
+  return db("resources").insert(project);
+}
+
+function remove() {
+  return db("projects"), where({ id }).del();
+}
