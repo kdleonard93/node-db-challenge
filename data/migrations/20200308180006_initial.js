90f948137568682eb43b919d8a5d62aabd90785c
Kyle Leonard <kdleonard@icloud.com>
2020-03-09 20:54:16 -0500

    Fixed a few bugs

--- data/migrations/20200308180006_initial.js
+++ data/migrations/20200308180006_initial.js
@@ -1,6 +1,6 @@
 exports.up = async function(knex) {
   await knex.schema.createTable("projects", table => {
-    table.increment("id").primaryKey();
+    table.increments("id").unique();
     table.text("name").notNull();
     table.text("description");
     table
@@ -10,7 +10,7 @@ exports.up = async function(knex) {
   });
 
   await knex.schema.createTable("resource", table => {
-    table.increment("id").unique();
+    table.increments("id").unique();
     table
       .text("name")
       .notNull()
@@ -19,7 +19,7 @@ exports.up = async function(knex) {
   });
 
   await knex.schema.createTable("tasks", table => {
-    table.increment("id").unique();
+    table.increments("id").unique();
     table.text("description").notNull();
     table.text("notes");
     table
